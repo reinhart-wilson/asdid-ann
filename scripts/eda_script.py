bf08b186d6b7f078aa05492f9c70cf4a2f3caccd
reinhart-wilson <6182001030@student.unpar.ac.id>
2024-11-18 17:16:30 +0700

    Create eda_script.py

---
+++ scripts/eda_script.py
@@ -0,0 +1,34 @@
+import sys
+import os
+
+# Tambahkan path proyek ke environment variable PYTHONPATH
+project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.append(project_root)
+
+from src.utils import eda_utils
+
+dataset_path = '../dataset/prepped_data'
+
+# Visualisasi persebaran data
+class_counts = eda_utils.count_data_per_class(dataset_path)
+sorted_counts = eda_utils.sort_counts(class_counts)
+counts = list(sorted_counts.values())
+color = [
+    'skyblue' if v >= 700 
+    else 'gold' if v >= 700 
+    else 'gold' for v in counts
+    ]
+eda_utils.visualize_class_counts(sorted_counts, 
+                                 plot_size=(8,6), 
+                                 threshold=800, 
+                                 in_percentage=True,
+                                 rotation=0,
+                                 horizontal = True,
+                                 color=color
+                                 )
+
+# Visualisasi persebaran ukuran citra
+eda_utils.eda_scatter_plot(dataset_path)
+
+
+
