94bb107f4d439021b10e477f1bb522bc6b87f793
reinhart-wilson <6182001030@student.unpar.ac.id>
2024-11-26 20:46:47 +0700

    Update squeezenet.py
    now can include top

--- src/mymodels/squeezenet.py
+++ src/mymodels/squeezenet.py
@@ -5,9 +5,12 @@ from .base_models import SqueezeNet
 
 class MySqueezeNet(AbstractCNN):   
     
-    def build_model(self, include_classification_head=True):            
+    def build_model(self, 
+                    include_classification_head=True, 
+                    include_top = False):   
+         
         base_model = SqueezeNet(
-            include_top=False, 
+            include_top=include_top, 
             weights=None,
             input_shape=self.input_shape,
             pooling='avg',
@@ -18,12 +21,15 @@ class MySqueezeNet(AbstractCNN):
         x = base_model.output
         
         # Tambahkan head klasifikasi jika diinginkan
-        if include_classification_head:
-            x = layers.Dense(self.num_classes, activation='softmax')(x)
-            self.classification_head = None
+        if include_top is not True:
+            if include_classification_head:
+                x = layers.Dense(self.num_classes, activation='softmax')(x)
+                self.classification_head = None
+            else:
+                self.classification_head = layers.Dense(self.num_classes, 
+                                                        activation='softmax')
         else:
-            self.classification_head = layers.Dense(self.num_classes, 
-                                                    activation='softmax')
+            self.classification_head = None
         
         # Buat model awal
         self.model = models.Model(inputs=base_model.input, outputs=x)
