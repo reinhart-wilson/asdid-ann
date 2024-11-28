9650b2551db1253fde09070b71b9783e1daa627d
reinhart-wilson <6182001030@student.unpar.ac.id>
2024-11-29 00:03:29 +0700

    Update exp_top_dense.py

--- scripts/mobilenetv2/exp_top_dense.py
+++ scripts/mobilenetv2/exp_top_dense.py
@@ -17,9 +17,11 @@ from tensorflow.keras import optimizers, metrics, callbacks, layers, regularizer
 from utils import training_utils as tutils
 
 # Params
+mode        = 0 # 0 cont 1 train
+last_epoch  = 0
 epoch       = 200
 lr          = 1e-4
-batch_size  = 16*6
+batch_size  = 12
 dense       = 512*2 # Currently tuning
 dropout     = 0
 weights     = None
@@ -43,7 +45,6 @@ LAST_MODEL_FILENAME     = 'model_at_{epoch}.tf'
 LATEST_MODEL_FILENAME   = 'latest_model.tf'
 LOGDIR                  = os.path.join(SAVE_PATH, "logs")
 
-
 # Load data
 train_data_dir = os.path.join(dinfo.DATA_PATH, 'train')
 val_data_dir = os.path.join(dinfo.DATA_PATH, 'validation')
@@ -55,17 +56,6 @@ val_datagen = gutils.make_datagen(val_data_dir,
                                   dinfo.IMG_RES, 
                                   batch_size,
                                   shuffle=False)
-
-# Definisi model
-model = MyMobileNetV2(dinfo.IMG_DIM, 
-                      dinfo.NUM_CLASSES, 
-                      dense_neuron_num=dense,
-                      dropout=dropout,
-                      weights = weights,
-                      weight_decay=wdecay,
-                      alpha=alpha
-                      )
-model.build_model()
 
 # Callbacks
 model_callbacks = [
@@ -80,34 +70,57 @@ model_callbacks = [
     # SaveLatestModel(os.path.join(SAVE_PATH, LATEST_MODEL_FILENAME))
 ]
 
-# Learning Rate
-if 'scheduler_config' in lr_config:
-    lr_scheduler_config = lr_config['scheduler_config']
-    if lr_scheduler_config['name'] == 'cosine_decay':
-        decay_steps = tutils.compute_decay_steps(
-            train_datagen.samples,                                                  
-            batch_size,                                                  
-            lr_scheduler_config['epochs_to_decay'])
-        lr = optimizers.schedules.CosineDecay(
-            initial_learning_rate=lr_config['init_value'],
-            alpha = lr_scheduler_config['lr_alpha'],
-            decay_steps = decay_steps
-        )
-else:
-    lr = lr_config['init_value']
+# Definisi model
+model = MyMobileNetV2(dinfo.IMG_DIM, 
+                      dinfo.NUM_CLASSES, 
+                      dense_neuron_num=dense,
+                      dropout=dropout,
+                      weights = weights,
+                      weight_decay=wdecay,
+                      alpha=alpha
+                      )
 
-# Compile
-optimizer = optimizers.Adam(learning_rate=lr)
-model_metrics = [
-    'accuracy',
-    metrics.Recall(name='recall')    
-    ]
-model.compile_model(optimizer=optimizer, metrics=model_metrics)
-
-# Train
-model.train(train_datagen,
-            val_datagen, 
-            epochs=epoch,
-            batch_size=batch_size,
-            callbacks=model_callbacks)
-model.save_model(os.path.join(SAVE_PATH, LAST_MODEL_FILENAME.format(epoch=epoch)))
+if mode == 1:
+    model.build_model()
+    
+    
+    # Learning Rate
+    if 'scheduler_config' in lr_config:
+        lr_scheduler_config = lr_config['scheduler_config']
+        if lr_scheduler_config['name'] == 'cosine_decay':
+            decay_steps = tutils.compute_decay_steps(
+                train_datagen.samples,                                                  
+                batch_size,                                                  
+                lr_scheduler_config['epochs_to_decay'])
+            lr = optimizers.schedules.CosineDecay(
+                initial_learning_rate=lr_config['init_value'],
+                alpha = lr_scheduler_config['lr_alpha'],
+                decay_steps = decay_steps
+            )
+    else:
+        lr = lr_config['init_value']
+    
+    # Compile
+    optimizer = optimizers.Adam(learning_rate=lr)
+    model_metrics = [
+        'accuracy',
+        metrics.Recall(name='recall')    
+        ]
+    model.compile_model(optimizer=optimizer, metrics=model_metrics)
+    
+    # Train
+    model.train(train_datagen,
+                val_datagen, 
+                epochs=epoch,
+                batch_size=batch_size,
+                callbacks=model_callbacks)
+    model.save_model(os.path.join(SAVE_PATH, LAST_MODEL_FILENAME.format(epoch=epoch)))
+if mode == 0:
+    model.load_model(os.path.join(SAVE_PATH, LATEST_MODEL_FILENAME))
+    model.train(train_datagen,
+                val_datagen, 
+                epochs=epoch,
+                batch_size=batch_size,
+                callbacks=model_callbacks,
+                initial_epoch=last_epoch)
+    
