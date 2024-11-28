103f7c4eb741772e75027b6eaa9c3fab5937e4a2
reinhart-wilson <6182001030@student.unpar.ac.id>
2024-11-28 19:59:54 +0700

    Update data_info.py

--- config/configs/other_configs/data_info.py
+++ config/configs/other_configs/data_info.py
@@ -1,9 +1,9 @@
-DATA_PATH = "../dataset/split_prepped_data"
-RESULT_PATH = "../training_result"
-ADDITIONAL_DATA_PATH = "../dataset/prepped_additional_data"
-ADDITIONAL_DATA__2_PATH = "../dataset/split_prepped_additional_data_2"
-BASE_PATH = "../dataset"
-EXTENDED_DATA_PATH = '../dataset/split_prepped_data _with_added_data_all_class'
+DATA_PATH = "../../dataset/split_prepped_data"
+RESULT_PATH = "../../training_result"
+ADDITIONAL_DATA_PATH = "../../dataset/prepped_additional_data"
+ADDITIONAL_DATA__2_PATH = "../../dataset/split_prepped_additional_data_2"
+BASE_PATH = "../../dataset"
+EXTENDED_DATA_PATH = '../../dataset/split_prepped_data _with_added_data_all_class'
 
 IMG_RES = (224, 224)  
 IMG_DIM = tuple(list(IMG_RES) + [3])
